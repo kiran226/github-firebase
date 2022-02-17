a462182f697ff899aac8232e9e299a20b6033a6c
kiran <gandikotakiran99@gmail.com>
2022-02-17 11:10:15 +0530

    fixing the navbar toggler issue

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -14,28 +14,37 @@ import {Link} from 'react-router-dom'
 import {UserContext} from '../context/UserContext'
 
 const Header=()=>{
+
+    const context = useContext(UserContext);
+    const[isOpen,setIsopen] =useState(false);
+
+    const toggle=()=>setIsopen(!isOpen);
+
     return(
         <Navbar color="info" light expand="md">
             <NavbarBrand>
                 <Link to="/" className="text-white">LCO gitfire app</Link>
             </NavbarBrand>
-            <NavbarToggler />
-            <Collapse navbar>
+            <NavbarText className="text-white">
+            {}
+            </NavbarText>
+             <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
                 <Nav className="ms-auto" navbar>
                     <NavItem>
-                        <NavLink className="text-white">
+                        <NavLink tag={Link} to="/signup" className="text-white">
                             Signup
                         </NavLink>
                     </NavItem>
 
                     <NavItem>
-                        <NavLink className="text-white">
+                        <NavLink tag={Link} to="/signin" className="text-white">
                             Signin
                         </NavLink>
                     </NavItem>
 
                     <NavItem>
-                        <NavLink className="text-white">
+                        <NavLink tag={Link} to="/" className="text-white">
                             Logout
                         </NavLink>
                     </NavItem>
